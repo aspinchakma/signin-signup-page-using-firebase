d119b2b72186ca97bdb4452598a9ddc6dbc10f38
Aspin Chakma <aspinchakma2019@gmail.com>
2025-09-08 00:18:23 +0600

    basic setup

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+
 const Root = () => {
   return (
-    <div>
-      <h2>This is root</h2>
+    <div className="min-h-screen flex flex-col lg:w-[80%] mx-auto">
+      <Header />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 };
